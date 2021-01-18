b4b944b68d2005a78da76adb8bff36a802b92a9f
Ciarán Murray <ciaran@murraynet.dk>
2021-01-18 22:43:20 +0100

    fixed err by removing netatmo function

--- src/index.js
+++ src/index.js
@@ -5,13 +5,3 @@ import "./css/main.css";
 // Say hello
 console.log("🦊 Hello! Edit me in src/index.js");
 
-var netatmo = require('./netatmo');
-
-var auth = {
-  "client_id": "",
-  "client_secret": "",
-  "username": "",
-  "password": "",
-};
-
-var api = new netatmo(auth);
